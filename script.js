0da58c7f3ef0530147393cc80b17284e309bfd44
matheus <matheus.reis.santos03@escola.pr.gov.br>
2025-06-09 08:41:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function somar () {
-    let n1 = 0;
-    let n2 = 0;
- // declarei a função e coloquei duas variáveis.
-    n1 = Number(window.prompt('Digite um número:'));
-    n2 = Number(window.prompt('Digite outro número:'));
-    let soma = n1 + n2 
-    alert(soma);
-// declarei uma função para cada e coloquei o calculo da soma delas.
-    if(soma >= 10){
-        alert("O resultado é maior que 10");
- } else {
-        alert("Não é maior que 10")
- }
-   // mostra se se o resultado é maior que 0 ou não.
+function calculo() {
+    let ano = 0;
+    let idade;
+//declarei a função e coloquei duas variáveis.
+    ano = Number(window.prompt('Digite seu ano de nascimento:'));
+       idade = 2025 - ano;
+    alert("sua idade é " + idade);
+//declarei uma função para cada variável e coloquei o calculo para descobrir a idade.
+       if( idade >18){
+              alert("Você pode tirar a carteira")
+       } else{
+              let dif = 18 - idade;
+       alert("Você ainda não pode tirar a carteira pois faltam " + dif + " para você ter 18 anos")
+       }
+       //mostra que se vc tem idade para tirar a carteira ou não e quanto tempo falta para ttirar ela.
 }
